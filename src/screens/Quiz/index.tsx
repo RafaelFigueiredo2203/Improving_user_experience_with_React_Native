c7bad91b7d0102c10d37409ea77e1acc5ef6575f
RafaelFigueiredo2203 <rafaelfigueiredojunior7@gmail.com>
2024-06-07 14:27:50 -0300

    Animating OverlayFeedback

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -35,6 +35,8 @@ export function Quiz() {
   const [quiz, setQuiz] = useState<QuizProps>({} as QuizProps);
   const [alternativeSelected, setAlternativeSelected] = useState<null | number>(null);
 
+  const [statusReply, setStatusReply] = useState(0)
+
   const { navigate } = useNavigation();
 
   const route = useRoute();
@@ -79,8 +81,10 @@ export function Quiz() {
     }
 
     if (quiz.questions[currentQuestion].correct === alternativeSelected) {
+      setStatusReply(1);
       setPoints(prevState => prevState + 1);
     }else{
+      setStatusReply(2);
       shakeAnimation();
     }
 
@@ -192,7 +196,7 @@ export function Quiz() {
   return (
     <View style={styles.container}>
 
-      <OverlayFeedback status={0}/>
+      <OverlayFeedback status={statusReply}/>
       <Animated.View style={fixedPropgressBarStyle}>
         <Text style={styles.title}>
           {quiz.title}
